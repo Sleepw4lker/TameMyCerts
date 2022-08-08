26982f12638be908a03f6a23cc33712786c5620a
Uwe Gradenegger <uwe@gradenegger.eu>
2022-08-08 09:10:40 +0200

    Modify Copyright notice

--- TameMyCerts/IPAddressExtensions.cs
+++ TameMyCerts/IPAddressExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2021 Uwe Gradenegger
+﻿// Copyright 2021 Uwe Gradenegger <uwe@gradenegger.eu>
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
